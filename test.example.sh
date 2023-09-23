3319e42dfb4d73ba64b8ed79345aea6c04b96690
HidemaruOwO <hidemal@outlook.jp>
2023-09-23 19:31:52 +0900

    🐛 Fix example scripts (README.md, start.example.sh, test.example.sh)

--- test.example.sh
+++ test.example.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ----- START BOT ENV ----- #
-export BOT_TOKEN=xxx
 export BOT_OWNER_ID=830789490481954856
+export BOT_TOKEN="xxx"
 export BOT_PREFIX="&&"
 # ----- STOP BOT ENV ------ #
-target/release/iris
+cargo run
