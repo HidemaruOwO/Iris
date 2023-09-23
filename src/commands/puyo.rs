6f135eebaec239ee85dfdfbcd1b8bc67e7abea20
HidemaruOwO <hidemal@outlook.jp>
2023-09-23 18:58:34 +0900

    ✨ Add puyo command (src/commands/puyo.rs)

---
+++ src/commands/puyo.rs
@@ -0,0 +1,14 @@
+use logger_rs::info;
+use serenity::model::prelude::*;
+use serenity::prelude::*;
+
+pub async fn main(context: &Context, message: &Message, _args: &Vec<&str>) {
+    message
+        .reply(
+            context,
+            "https://obj.04.si/files/b3029ce7-35ef-4e0d-a6ea-7f05ffb3cb71.gif",
+        )
+        .await
+        .unwrap();
+    info!("✅ Successful");
+}
