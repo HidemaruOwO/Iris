dc25e19fea00421680c21f1315cfac0bfdaee065
HidemaruOwO <hidemal@outlook.jp>
2023-12-03 18:53:44 +0900

    🔧 ConfigにBOTのIDを追加する項目の追加 (src/libs/config.rs, start.example.sh, test.example.sh)

--- start.example.sh
+++ start.example.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # ----- START BOT ENV ----- #
 export BOT_OWNER_ID=830789490481954856
+export BOT_ID=1153958446148898836
 export BOT_TOKEN="xxx"
 export BOT_PREFIX="&&"
 # ----- STOP BOT ENV ------ #
